177695eea64f7bfae30a45999a73d701f68737cf
Etec 244 <bianca.pinheiro2@etec.sp.gov.br>
2021-09-17 15:03:49 -0300

    kkkkkkk

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
+
+async presentAlert() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Alert',
+    subHeader: 'Subtitle',
+    message: 'olá amor .',
+    buttons: ['OK']});
+
+  await alert.present();
+
+  const { role } = await alert.onDidDismiss();
+  console.log('onDidDismiss resolved with role', role);}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'o noah é lindo.',
+      duration: 2000
+    });
+    toast.present();
+  }
 }
+
